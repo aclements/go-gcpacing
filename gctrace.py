b00b60a46419a5a86f81ec5a4d5d844532f19715
Austin Clements <austin@google.com>
2015-04-21 14:50:50 -0400

    gctrace: be robust to new parts of gctrace line
    Currently, gctrace uses a single monolithic regexp to parse the
    gctrace line. This isn't robust to changes in or additions to the
    format.
    
    Instead, break the line into parts on commas and parse each part
    individually. If we encounter an unknown part, print a warning and
    move on.

--- gctrace.py
+++ gctrace.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import sys
 import re
 
 heapMinimum = 4<<20
@@ -12,45 +13,63 @@ def parse(fp, GOGC=None, omit_forced=True):
     If GOGC is not None, records will include a computed H_g."""
 
     r = re.compile(r'gc #(?P<n>[0-9]+) @(?P<end>[0-9.]+)s (?P<util>[0-9]+)%: '
-                   r'(?P<clocks>[+0-9]+) ms clock, '
-                   r'(?P<cpus>[+/0-9]+) ms cpu, '
-                   r'(?P<H_T>[0-9]+)->(?P<H_a>[0-9]+)->(?P<H_m>[0-9]+) MB, '
-                   r'(?P<gomaxprocs>[0-9]+) P')
+                   r'(?P<parts>.*)')
     H_m_prev = None
     for line in fp:
         m = r.match(line)
         if not m:
             continue
         d = m.groupdict()
+        d['n'] = int(d['n'])
+        d['end'] = _sec(d['end'])
+        d['util'] = int(d['util']) / 100
         d['forced'] = '(forced)' in line
-        for k, v in list(d.items()):
-            if k == 'clocks':
-                v = list(map(_ms, v.split('+')))
+
+        # Process parts
+        for part in d.pop('parts').split(','):
+            part = part.strip()
+
+            m = re.match(r'([+0-9]+) ms clock', part)
+            if m:
+                v = list(map(_ms, m.group(1).split('+')))
                 d['clocks'] = v
                 d['clocksSTW'] = v[::2]
                 d['clocksCon'] = v[1::2]
-            elif k == 'cpus':
-                phases = v.split('+')
-                scan = phases[3]
+                continue
+
+            m = re.match(r'([+/0-9]+) ms cpu', part)
+            if m:
+                phases = m.group(1).split('+')
                 d['markPhase'] = 3
-                if '/' in scan:
+                mark = phases[d['markPhase']]
+                if '/' in mark:
                     d['cpu_assist'], d['cpu_bg'], d['cpu_idle'] \
-                        = map(_ms, scan.split('/'))
+                        = map(_ms, mark.split('/'))
                     phases[3] = (d['cpu_assist'] + d['cpu_bg']) * 1e3
                 else:
-                    d['cpu_bg'] = _ms(scan)
+                    d['cpu_bg'] = _ms(mark)
                     d['cpu_assist'] = d['cpu_idle'] = 0
                 v = list(map(_ms, phases))
                 d['cpus'] = v
                 d['cpusSTW'] = v[::2]
                 d['cpusCon'] = v[1::2]
-            elif k == 'end':
-                d['end'] = _sec(v)
-            else:
-                v = int(v)
-                if k.startswith('H_'):
-                    v <<= 20
-                d[k] = v
+                continue
+
+            m = re.match(r'(?P<H_T>[0-9]+)->(?P<H_a>[0-9]+)->(?P<H_m>[0-9]+) MB', part)
+            if m:
+                for k, v in m.groupdict().items():
+                    v = int(v) << 20
+                    d[k] = v
+                continue
+
+            m = re.match(r'([0-9]+) P', part)
+            if m:
+                d['gomaxprocs'] = int(m.group(1))
+                continue
+
+            print('unexpected part in GC line: %s' % part, file=sys.stderr)
+
+        # Compute derived fields
         d['start'] = d['end'] - sum(d['clocks'])
         if GOGC is not None:
             if H_m_prev is None:
